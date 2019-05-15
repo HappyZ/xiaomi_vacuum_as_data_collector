9a42a6c53c83467476cdee919f9abdb936161dcd
HappyZ <zhuyanzi@gmail.com>
2019-05-15 13:29:23 -0500

    Update pull_data_from_phone.sh

--- pull_data_from_phone.sh
+++ pull_data_from_phone.sh
@@ -5,12 +5,22 @@ LOCALFOLDER=$2
 
 mkdir -p $LOCALFOLDER
 
+if [[ ! $? -eq 0 ]]; then
+    echo "Cannot create folder $LOCALFOLDER"
+    exit $?;
+fi
+
+
 for file in $(adb shell "ls $PHONEPATH" | tr -d '\r'); do
     if [[ $PHONEPATH = *"*" ]]; then
         adb pull "${PHONEPATH%%/*}/$file" $LOCALFOLDER/
-        adb shell rm "${PHONEPATH%%/*}/$file"
+        if [[ $? -eq 0 ]]; then
+            adb shell rm "${PHONEPATH%%/*}/$file"
+        fi
     else
         adb pull "$PHONEPATH/$file" $LOCALFOLDER/
-        adb shell rm "$PHONEPATH/$file"
+        if [[ $? -eq 0 ]]; then
+            adb shell rm "$PHONEPATH/$file"
+        fi
     fi
 done
